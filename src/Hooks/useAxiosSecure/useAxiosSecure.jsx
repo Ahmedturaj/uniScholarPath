0c0fdf3062886d6957645f6caf645beffdcbc12a
Ahmedturaj <ahmedturaj12@gmail.com>
2024-06-01 08:31:53 +0600

    added useAxiosSecure hooks

---
+++ src/Hooks/useAxiosSecure/useAxiosSecure.jsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../Provider/AuthProvider";
+
+const axiosSecure = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
+});
+
+function useAxiosSecure() {
+    const { logOut } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        axiosSecure.interceptors.response.use(
+            (res) => {
+                return res;
+            },
+            (error) => {
+                if (error.response.status === 401 || error.response.status === 403) {
+                    logOut()
+                        .then(() => {
+                            navigate("/logIn");
+                        })
+                        .catch((error) => console.error(error));
+                }
+            }
+        );
+    }, [logOut, navigate]);
+
+    return axiosSecure;
+}
+
+export default useAxiosSecure;
